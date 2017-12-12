04344cebfca194421783d1965a6af87876b4a85b
schekey <schekey@163.com>
2017-12-12 21:39:43 +0800

    初步

---
+++ src/main/java/com/schekey/saleonline/SaleonlineApplication.java
@@ -0,0 +1,12 @@
+package com.schekey.saleonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleonlineApplication.class, args);
+	}
+}
